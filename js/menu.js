954398d91984af3c02e7e702ef977b1540e8316a
NgmThanh <nmthanh99@gmail.com>
2020-06-15 01:51:16 +0200

    skills page added, animation box works

--- js/menu.js
+++ js/menu.js
@@ -2,11 +2,13 @@
 
 // get elems on click
 var about = document.getElementById("about-menu");
+var skills = document.getElementById("skills-menu");
 var works = document.getElementById("works-menu");
 var contact = document.getElementById("contact-menu");
 
 // get sections of page
 var aboutPage = document.getElementById('about');
+var skillsPage = document.getElementById('skills');
 var worksPage = document.getElementById('works');
 var contactPage = document.getElementById('contact');
 
@@ -14,6 +16,16 @@ var contactPage = document.getElementById('contact');
 about.addEventListener('click', () => {
 
     aboutPage.style.display = "block";
+    skillsPage.style.display = "none";
+    worksPage.style.display = "none";
+    contactPage.style.display = "none";
+
+});
+
+skills.addEventListener('click', () => {
+
+    aboutPage.style.display = "none";
+    skillsPage.style.display = "block";
     worksPage.style.display = "none";
     contactPage.style.display = "none";
 
@@ -22,6 +34,7 @@ about.addEventListener('click', () => {
 works.addEventListener('click', () => {
 
     aboutPage.style.display = "none";
+    skillsPage.style.display = "none";
     worksPage.style.display = "block";
     contactPage.style.display = "none";
 });
@@ -29,6 +42,7 @@ works.addEventListener('click', () => {
 contact.addEventListener('click', () => {
 
     aboutPage.style.display = "none";
+    skillsPage.style.display = "none";
     worksPage.style.display = "none";
     contactPage.style.display = "block";
 
