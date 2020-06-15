036df86defd125ce31fb26207d1a86146176395f
NgmThanh <nmthanh99@gmail.com>
2020-06-15 17:00:35 +0200

    progess bar

--- js/menu.js
+++ js/menu.js
@@ -67,3 +67,23 @@ contact.addEventListener('click', () => {
     // contactPage.style.display = "block";
 
 });
+
+window.onload = () => {
+    // Ecouteur d'évènement sur scroll
+    window.addEventListener("scroll", () => {
+        // Calcul de la hauteur "utile" du document
+        let hauteur = document.documentElement.scrollHeight - window.innerHeight
+
+        // Récupération de la position verticale
+        let position = window.scrollY
+
+        // Récupération de la largeur de la fenêtre
+        let largeur = document.documentElement.clientWidth
+
+        // Calcul de la largeur de la barre
+        let barre = position / hauteur * largeur
+
+        // Modification du CSS de la barre
+        document.getElementById("progress").style.width = barre+"px"
+    })
+}
