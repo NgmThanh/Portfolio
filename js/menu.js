b965126c9d32b4b8986ff814cb6ea616bf568bab
NgmThanh <nmthanh99@gmail.com>
2020-06-15 23:04:39 +0200

    js menu

--- js/menu.js
+++ js/menu.js
@@ -1,73 +1,26 @@
 // ============ SCRIPT MENU ============ //
 
-// get elems on click
-var about = document.getElementById("about-menu");
-var skills = document.getElementById("skills-menu");
-var works = document.getElementById("works-menu");
-var contact = document.getElementById("contact-menu");
+// html collection of class "menu--text"
+var classMenu = document.getElementsByClassName('menu--text');
+var arrayMenu = new Array();
 
-// get sections of page
-var aboutPage = document.getElementById('about');
-var skillsPage = document.getElementById('skills');
-var worksPage = document.getElementById('works');
-var contactPage = document.getElementById('contact');
+// array menu items (except dark mode button)
+for(var i = 0; i < classMenu.length-1; i++){
+    arrayMenu.push(classMenu[i]);
+}
 
-// events 
-about.addEventListener('click', () => {
-
-    about.style.color = "#FF5711";
-    skills.style.color = "whitesmoke";
-    works.style.color = "whitesmoke";
-    contact.style.color = "whitesmoke";
-
-    // aboutPage.style.display = "block";
-    // skillsPage.style.display = "none";
-    // worksPage.style.display = "none";
-    // contactPage.style.display = "none";
-
+// set event listener in all menu buttons
+document.querySelectorAll('.menu--text').forEach(itemMenu => {
+    
+    itemMenu.addEventListener('click', () => {
+        arrayMenu.forEach(element => {
+            element.style.color = "";
+        });
+        itemMenu.style.color = "#FF5711";
+    });
 });
 
-skills.addEventListener('click', () => {
-
-    about.style.color = "whitesmoke";
-    skills.style.color = "#FF5711";
-    works.style.color = "whitesmoke";
-    contact.style.color = "whitesmoke";
-
-    // aboutPage.style.display = "none";
-    // skillsPage.style.display = "block";
-    // worksPage.style.display = "none";
-    // contactPage.style.display = "none";
-
-});
-
-works.addEventListener('click', () => {
-
-    about.style.color = "whitesmoke";
-    skills.style.color = "whitesmoke";
-    works.style.color = "#FF5711";
-    contact.style.color = "whitesmoke";
-
-    // aboutPage.style.display = "none";
-    // skillsPage.style.display = "none";
-    // worksPage.style.display = "block";
-    // contactPage.style.display = "none";
-});
-
-contact.addEventListener('click', () => {
-
-    about.style.color = "whitesmoke";
-    skills.style.color = "whitesmoke";
-    works.style.color = "whitesmoke";
-    contact.style.color = "#FF5711";
-
-    // aboutPage.style.display = "none";
-    // skillsPage.style.display = "none";
-    // worksPage.style.display = "none";
-    // contactPage.style.display = "block";
-
-});
-
+// progress bar in scrolling
 window.onload = () => {
     // Ecouteur d'évènement sur scroll
     window.addEventListener("scroll", () => {
@@ -85,5 +38,5 @@ window.onload = () => {
 
         // Modification du CSS de la barre
         document.getElementById("progress").style.width = barre+"px"
-    })
-}
+    });
+};
