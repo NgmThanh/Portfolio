c7728a845defe8915ee8cef336619e58068f1443
NgmThanh <nmthanh99@gmail.com>
2020-06-16 01:48:05 +0200

    project window on click

---
+++ js/works.js
@@ -0,0 +1,11 @@
+document.getElementsByClassName('timeline__box').onclick=function(){
+    // Remove any element-specific value, falling back to stylesheets
+    console.log('ok');
+
+
+
+
+
+    document.getElementById('divToShow').style.display='block';
+};
+
