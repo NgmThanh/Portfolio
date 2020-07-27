61928c962dde09fcbba89c66a6e3e5d5e84ebf8b
NgmThanh <nmthanh99@gmail.com>
2020-07-27 19:47:14 +0200

    apparition on scroll (bug z-index svg decorations)

--- js/scroll.js
+++ js/scroll.js
@@ -1,25 +1,22 @@
-const threshold = .1
+const ratio = 0.1;
+
 const options = {
-root: null,
-rootMargin: '0px',
-threshold
+    root: null,
+    rootMargin: '0px',
+    threshold: ratio
 }
 
 const handleIntersect = function (entries, observer) {
     entries.forEach(function (entry) {
-        if (entry.intersectionRatio > threshold) {
+        if (entry.intersectionRatio > ratio) {
             entry.target.classList.remove('reveal')
-            observer.unobserve(entry.target)
+        } else {
+            entry.target.classList.add('reveal')
         }
-    })
+    });
 }
 
-document.documentElement.classList.add('reveal-loaded')
-
-window.addEventListener("DOMContentLoaded", function () {
-const observer = new IntersectionObserver(handleIntersect, options)
-const targets = document.querySelectorAll('.reveal')
-targets.forEach(function (target) {
-    observer.observe(target)
-})
+const observer = new IntersectionObserver(handleIntersect, options);
+document.querySelectorAll('.reveal').forEach(function (r) {
+    observer.observe(r)
 })
